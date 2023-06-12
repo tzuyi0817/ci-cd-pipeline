e048aa79d680d0cae18755d0fc81108daee3d1ea
tzuyi <a0988426059@gmail.com>
2023-06-12 15:28:27 +0800

    fix: service work cache optimization with stale while revalidate

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,45 +1,47 @@
-// pnpm install --save-dev sw-precache
+// pnpm install --save-dev sw-precache or workbox
 
 const filesToCache = [
 	'/',
-  // '/styles/globals.css',
-  // '/styles/Home.module.css',
 ];
 const cacheName = 'static-v1';
 const dataCacheName = 'static-v1-data';
 
 self.addEventListener('install', event => {
   event.waitUntil(
-		caches.open(cacheName).then(cache => {
-			return cache.addAll(filesToCache);
-		})
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
 	);
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-		caches.keys().then(cacheNames => {
-			const promiseArr = cacheNames.map(item => {
-				if (item !== cacheName) {
-					return caches.delete(item);
-				}
-			})
-			return Promise.all(promiseArr);
-		})
+    caches.keys().then(cacheNames => {
+      const promiseArr = cacheNames.map(item => {
+        if (item !== cacheName) {
+          return caches.delete(item);
+        }
+      })
+      return Promise.all(promiseArr);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
-	event.respondWith(
-		caches.match(event.request).then(response => {
-			return response || fetch(event.request).then(res =>
-				caches.open(dataCacheName).then(cache => {
-          // cache.put(key, value)
-          // 下一次 caches.match 會對應到 event.request
-          cache.put(event.request, res.clone());
-          return res;
-				})
-			);
-		})
-	);
+  const cached = caches.match(event.request);
+  const fetched = fetch(event.request);
+  const fetchedCopy = fetched.then(resp => resp.clone());
+
+  event.respondWith(
+    Promise.race([fetched.catch(_ => cached), cached])
+      .then(response => response || fetched)
+      .catch(_ => new Response(null, { status: 404 }))
+  );
+
+  // 用 fetch 回來的資料更新快取
+  event.waitUntil(
+    Promise.all([fetchedCopy, caches.open(dataCacheName)])
+      .then(([response, cache]) => cache.put(event.request, response))
+      .catch(_ => {/* errors */})
+  );
 });
